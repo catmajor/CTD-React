694b3ce6e83490e3a3f895d9f5b0286d2a279ead
Nikita Ostapenko <nikitul2005@gmail.com>
2025-07-22 21:01:45 -0400

    move todo code from app to todolist

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,5 +1,17 @@
 function TodoList(){
-    return
+    const todos = [
+    {id: 1, title: "review resources"},
+    {id: 2, title: "take notes"},
+    {id: 3, title: "code out app"},
+    ]
+    return (
+        <div>
+            <h1>Todo List</h1>
+            <ul>
+                {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
+            </ul>
+        </div>
+    );
 }
 
 export default TodoList
