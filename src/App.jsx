d01eba5e60e16c136ea789f1dd4eee83297e6e6f
Nikita Ostapenko <nikitul2005@gmail.com>
2025-07-28 13:20:42 -0400

    add newtodo text hook to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import {useState} from 'react'
 import './App.css'
 import TodoList from './TodoList'
 import TodoForm from './TodoForm'
 function App() {
+  const [newTodo, setNewTodo] = useState("Example Text");
   return(
     <>
       <h1>Todo List</h1>
       <TodoForm/>
+      {newTodo}
       <TodoList/>
     </>
   );
