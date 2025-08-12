e03d1629f77792b9849764e636e9d7b9ca589075
Nikita Ostapenko <nikitul2005@gmail.com>
2025-08-12 18:10:07 -0400

    Part 1 make todoform actually add items to the todolist

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,9 +1,18 @@
-function TodoForm() {
+import { useRef } from 'react';
+function TodoForm({onAddTodo}) {
+    const todoTitleInput = useRef('');
+    function handleAddTodo(event) {
+        const title = event.target.title.value
+        if (!title.trim()) return
+        onAddTodo(title)
+        event.target.title.value = ''
+        todoTitleInput.current.focus()
+    }
     return (
         <div>
-            <form>
+            <form onSubmit = {handleAddTodo}>
                 <label htmlFor="todoTitle">Todo</label>
-                <input type="text" id="todoTitle" />
+                <input type="text" id="todoTitle" name="title"/>
                 <button type="submit">Add Todo</button>
             </form>
         </div>
