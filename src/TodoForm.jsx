80c27238b0dc9a5ed38fde7ce6300c25bbb86329
Nikita Ostapenko <nikitul2005@gmail.com>
2025-08-12 18:16:47 -0400

    fix some things i forgot to add

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 function TodoForm({onAddTodo}) {
     const todoTitleInput = useRef('');
     function handleAddTodo(event) {
+        event.preventDefault()
         const title = event.target.title.value
         if (!title.trim()) return
         onAddTodo(title)
@@ -12,7 +13,7 @@ function TodoForm({onAddTodo}) {
         <div>
             <form onSubmit = {handleAddTodo}>
                 <label htmlFor="todoTitle">Todo</label>
-                <input type="text" id="todoTitle" name="title"/>
+                <input type="text" id="todoTitle" name="title" ref={todoTitleInput}/>
                 <button type="submit">Add Todo</button>
             </form>
         </div>
