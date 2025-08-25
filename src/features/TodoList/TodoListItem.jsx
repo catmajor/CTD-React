5735ab5bbe816b0da1d6be05eadf071ec25400c8
Nikita Ostapenko <nikitul2005@gmail.com>
2025-08-24 22:39:30 -0400

    add update button

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -10,15 +10,22 @@ function TodoListItem({todo, onCompleteTodo, onUpdateTodo}) {
   function handleEdit (event) {
     setWorkingTitle(event.target.value)
   }
+  function handleUpdate(event) {
+    if (isEditing === false) return
+    event.preventDefault()
+    onUpdateTodo({...todo, title: workingTitle})
+    setIsEditing(false)
+  }
   return (
     <li>
       {isEditing ? 
         <>
           <TextInputWithLabel value = {workingTitle} onChange={(e) => handleEdit(e)}/>
           <button type = "button" onClick = {() => handleCancel()}>Cancel</button>
+          <button type = "button" onClick = {(e) => handleUpdate(e)}>Update</button>
         </>
       :
-        <form>
+        <form onSubmit = {(e) => handleUpdate(e)}>
           <input type = "checkbox" checked = {todo.isComplete} onChange = {() => onCompleteTodo(todo.id)}></input>
           <span onClick = {() => setIsEditing(true)}>{todo.title}</span>
         </form>
