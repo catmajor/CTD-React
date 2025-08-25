8dd8944f90898cd088ed9ad5ebc421279a1e624e
Nikita Ostapenko <nikitul2005@gmail.com>
2025-08-24 22:03:10 -0400

    complete textinputwithlabel

--- src/shared/TxtInputWithLabel.jsx
+++ src/shared/TxtInputWithLabel.jsx
@@ -1,8 +1,20 @@
-function TxtInputWithLabel () {
-    return (
+function TextInputWithLabel({
+  elementId,
+  label,
+  onChange,
+  ref,
+  value,
+}) {
+  return (
     <>
-      <label></label>
-      <input></input>
+      <label htmlFor={elementId}>{label}</label>
+      <input
+        type="text"
+        id={elementId}
+        ref={ref}
+        value={value}
+        onChange={onChange}
+      />
     </>
   );
 }
