45881c115b97e6b676ee300e6ed40a7aaf603b99
Nikita Ostapenko <nikitul2005@gmail.com>
2025-08-24 22:24:18 -0400

    add cancel button

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -2,10 +2,18 @@ import { useState } from "react";
 import TextInputWithLabel from "../../shared/TextInputWithLabel";
 function TodoListItem({todo, onCompleteTodo}) {
   const [isEditing, setIsEditing] = useState(false);
+  const [workingTitle, setWorkingTitle] = useState(todo.title);
+  function handleCancel () {
+    setWorkingTitle(todo.title)
+    setIsEditing(false)
+  }
   return (
     <li>
       {isEditing ? 
-        <TextInputWithLabel value = {todo.title}/>
+        <>
+          <TextInputWithLabel value = {todo.title}/>
+          <button type = "button" onClick = {() => handleCancel()}>Cancel</button>
+        </>
       :
         <form>
           <input type = "checkbox" checked = {todo.isComplete} onChange = {() => onCompleteTodo(todo.id)}></input>
