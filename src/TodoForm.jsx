de723a3756d5b688343b3bde94336006129e8425
Nikita Ostapenko <nikitul2005@gmail.com>
2025-08-24 21:35:47 -0400

    change to controlled form

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,19 +1,18 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 function TodoForm({onAddTodo}) {
     const todoTitleInput = useRef('');
+    const [workingTodoTitle, setWorkingTodoTitle] = useState('');
     function handleAddTodo(event) {
         event.preventDefault()
-        const title = event.target.title.value
-        if (!title.trim()) return
-        onAddTodo(title)
-        event.target.title.value = ''
+        onAddTodo(workingTodoTitle)
+        setWorkingTodoTitle('')
         todoTitleInput.current.focus()
     }
     return (
         <div>
             <form onSubmit = {handleAddTodo}>
                 <label htmlFor="todoTitle">Todo</label>
-                <input type="text" id="todoTitle" name="title" ref={todoTitleInput}/>
+                <input type="text" id="todoTitle" name="title" ref={todoTitleInput} value = {workingTodoTitle} onChange = {(e) => setWorkingTodoTitle(e.target.value)}/>
                 <button type="submit">Add Todo</button>
             </form>
         </div>
