f77007f6cfebbf7569f8bd7e98fe1997ddbdabec
Nikita Ostapenko <nikitul2005@gmail.com>
2025-08-24 21:09:40 -0400

    add conditional rendering

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,12 +2,15 @@ import TodoListItem from "./TodoListItem";
 function TodoList({todoList}){
     return (
         <div>
-            <h1>Todo List</h1>
-            <ul>
+            {todoList.length == 0 ? 
+                <p>Add todo above to get started</p>
+            :
+                <ul>
                 {todoList.map(todo => 
                 <TodoListItem key={todo.id} title={todo.title} />
                 )}
             </ul>
+            }
         </div>
     );
 }
