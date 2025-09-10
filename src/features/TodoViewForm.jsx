c923edde77c85ba452ea109bd73cf24afef6740d
Nikita Ostapenko <nikitul2005@gmail.com>
2025-09-09 23:41:16 -0400

    add props to todoviewform and make it work

--- src/features/TodoViewForm.jsx
+++ src/features/TodoViewForm.jsx
@@ -1,17 +1,20 @@
-function TodoViewForm() {
+function TodoViewForm({ sortDirection, setSortDirection, sortField, setSortField }) {
+    const preventRefresh = (e) => { e.preventDefault(); }
   return (
-    <div>
-        <label>Sort By&nbsp;</label>
-        <select>
-            <option value="title">Title</option>
-            <option value="createdTime">Created Time</option>
-        </select>
-        <label>Sort Direction&nbsp;</label>
-        <select>
-            <option value="asc">Ascending</option>
-            <option value="desc">Descending</option>
-        </select>
-    </div>
+    <form onSubmit={preventRefresh}>
+        <div>
+            <label>Sort By&nbsp;</label>
+            <select value={sortField} onChange={(e) => setSortField(e.target.value)}>
+                <option value="title">Title</option>
+                <option value="createdTime">Created Time</option>
+            </select>
+            <label>Sort Direction&nbsp;</label>
+            <select value={sortDirection} onChange={(e) => setSortDirection(e.target.value)}>
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
+            </select>
+        </div>
+    </form>
 );
 }
 export default TodoViewForm;
