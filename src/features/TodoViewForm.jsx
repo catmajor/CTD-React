31107857d7611df70f628757fe38f9ef00b1c63d
Nikita Ostapenko <nikitul2005@gmail.com>
2025-09-09 23:33:21 -0400

    fill in todoviewform to have basic functionailty

--- src/features/TodoViewForm.jsx
+++ src/features/TodoViewForm.jsx
@@ -1,4 +1,17 @@
 function TodoViewForm() {
-  return <div>TodoViewForm</div>;
+  return (
+    <div>
+        <label>Sort By&nbsp;</label>
+        <select>
+            <option value="title">Title</option>
+            <option value="createdTime">Created Time</option>
+        </select>
+        <label>Sort Direction&nbsp;</label>
+        <select>
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+        </select>
+    </div>
+);
 }
 export default TodoViewForm;
